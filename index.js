81a6e07c2ac07602653f87ef73903be6d30aec78
phuvuong95 <47914479+phuvuong95@users.noreply.github.com>
2019-10-22 10:52:58 +0700

    Query parameters

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+    { id: 1, name: 'Thinh' },
+    { id: 2, name: 'Hung' }
+];
+
 app.get('/', function(req, res){  //get request: lấy dữ liệu và hiển thị lên trình duyệt
     res.render('index', {
         name: "AAA"
@@ -14,10 +19,18 @@ app.get('/', function(req, res){  //get request: lấy dữ liệu và hiển th
 
 app.get('/users', function(req, res){  //get request: lấy dữ liệu và hiển thị lên trình duyệt
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Thinh' },
-            { id: 2, name: 'Hung'}
-        ]
+        users: users
+    });
+});
+
+app.get('/users/search', function(req, res){
+    var q = req.query.q;
+    var matchedUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    
+    res.render('users/index',{
+       users: matchedUsers 
     });
 });
 
